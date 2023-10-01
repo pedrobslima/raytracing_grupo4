2879d3de4c84dfff2803de05d2e1ba813b6c28e7
rmls2 <millerrobert182@gmail.com>
2023-10-01 18:09:48 -0300

    subindo repositorio

---
+++ cli.py
@@ -0,0 +1,44 @@
+import argparse
+import json
+import os
+from build import Build
+from elements import set_elements
+from image import Image
+
+
+def cli() -> str:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-j', '--json-file', type=str, required=True,
+                        help='Escreva o arquivo json correpondente a imagem que será processada, no formato <file.json>.')
+
+    arg = parser.parse_args()
+    _file = arg.json_file
+
+    return _file
+
+
+def readinfo(_file: str) -> dict:
+    directory = os.getcwd()
+
+    path = f'{directory}\\inputs\\{_file}'
+    with open(path, 'r') as json_file:
+        info = json.load(json_file)
+
+    return info
+
+
+if __name__ == "__main__":
+    file: str = cli()
+    scene_info: dict = readinfo(file)
+
+    # setting object and info in the scene
+    objects, lights = set_elements(scene_info['objects'], scene_info['lights'])
+    scene_info['object_list'] = objects
+    scene_info['lights'] = lights
+
+    scene = Build(scene_info)
+    # building scene elements
+    image: Image = scene.render_scene()
+    # draw the image
+    image.draw_image(file)
---
+++ objects.py
@@ -0,0 +1,164 @@
+from math import sqrt, inf, hypot
+from ray import Ray
+import numpy as np
+
+
+class Object:
+    def __init__(self, _objects) -> None:
+        self.objects = _objects
+
+    def intersect(self):
+        '''This function will be recreated for other class'''
+        pass
+
+    def normal(self):
+        pass
+
+
+class Sphere(Object):
+
+    def __init__(self, objects, center, radius, color, ka, kd, ks, exp,
+                 kr, kt, index_of_refraction, tl=0, tr=0) -> None:
+        super().__init__(objects)
+        self.center = np.array(center)
+        self.radius = radius
+        self.color = color
+        self.ka = ka
+        self.kd = kd
+        self.ks = ks
+        self.exp = exp
+        self.kr = kr
+        self.kt = kt
+        self.index_of_refraction = index_of_refraction
+        self.tl = tl  # first parameter
+        self.tr = tr  # second parameter
+
+    def intersect(self, ray: Ray):
+
+        ray_to_sphere = self.center - ray.origin  # ray origin to sphere origin
+        t_min = np.dot(ray_to_sphere, ray.direction)  # parameter
+        distance = sqrt(np.dot(ray_to_sphere, ray_to_sphere) - t_min ** 2)
+
+        if distance ** 2 <= self.radius ** 2:  # d**2 <= radius**2?
+            h = sqrt(self.radius ** 2 - distance ** 2)  # pitagoras
+            tl, tr = t_min - h, t_min + h
+            #-if tl < 0:
+            #-    return inf if tr < 0 else tr
+            if tl < 0:
+                return tr if tr >= 0 else inf
+            #+
+            else:
+                return tl
+            
+        else:  # not intersection
+            return inf
+
+    def normal(self, P):
+        def norm(x, y, z): return hypot(x, y, z)
+        def normalize(a, b): return a / b
+        normal = P - self.center
+
+        return normalize(normal, norm(*normal))
+
+
+class Plane(Object):
+
+    def __init__(self, objects: Object, point: list, v_normal: list, color, ka, kd, ks, exp,
+                 kr, kt, index_of_refraction) -> None:
+        super().__init__(objects)
+        self.point = np.array(point)
+        self.v_normal = np.array(v_normal)
+        self.color = color
+        self.ka = ka
+        self.kd = kd
+        self.ks = ks
+        self.exp = exp
+        self.kr = kr
+        self.kt = kt
+        self.index_of_refraction = index_of_refraction
+
+    def intersect(self, ray: Ray):
+
+        v = np.dot(ray.direction, self.v_normal)
+        EPSLON = 0.0000001
+
+        if abs(v) < EPSLON:
+            return inf
+        else:
+            h = np.dot(self.point - ray.origin, self.v_normal)
+            t_parameter = h / v
+            #- return inf if t_parameter < EPSLON else t_parameter
+            if t_parameter < EPSLON:
+                return inf
+            return t_parameter
+            #-
+
+    def normal(self, _):
+        def norm(x, y, z): return hypot(x, y, z)
+        def normalize(a, b): return a / b
+
+        return normalize(self.v_normal, norm(*self.v_normal))
+
+
+class Triangle(Object):
+
+    def __init__(self, objects, coords: list[list], color, ka, kd, ks, exp,
+                 kr, kt, index_of_refraction) -> None:
+        super().__init__(objects)
+        self.point_A, self.point_B, self.point_C = np.array(coords[0]), \
+            np.array(coords[1]), \
+            np.array(coords[2])
+        self.color = color
+        self.ka = ka
+        self.kd = kd
+        self.ks = ks
+        self.exp = exp  # phong exponent
+        self.kr = kr
+        self.kt = kt
+        self.index_of_refraction = index_of_refraction
+
+        # calculating normal vector to the plane
+        u = self.point_B - self.point_A
+        v = self.point_C - self.point_A
+        self.v_normal = np.cross(u, v)
+
+        # pre processing
+        def projection(a, b): return (((np.dot(a, b)) / (np.dot(b, b))) * b)
+        h_b = u - projection(u, v)
+        h_c = v - projection(v, u)
+
+        self.h_b = h_b / (np.dot(h_b, h_b))
+        self.h_c = h_c / (np.dot(h_c, h_c))
+
+    def intersect(self, ray: Ray):
+        # plane intersection function
+        v = np.dot(ray.direction, self.v_normal)
+        EPSLON = 0.0000001
+
+        if abs(v) < EPSLON:  # parallel to the plane
+            t = inf
+        else:
+            h = np.dot(self.point_A - ray.origin, self.v_normal)
+            t_parameter = h / v
+            t = inf if t_parameter < 0 else t_parameter
+
+        if t == inf:  # not intersection
+            return inf
+
+        # calculation intersection point
+        P = ray.origin + (t * ray.direction)
+        vector_v = P - self.point_A
+        beta = np.dot(vector_v, self.h_b)
+        gama = np.dot(vector_v, self.h_c)
+        alpha = 1 - (beta + gama)
+
+        if alpha < 0 or beta < 0 or gama < 0:
+            return inf
+        else:
+            return t  # return escalar
+
+    def normal(self, _):
+        def norm(x, y, z): return hypot(x, y, z)
+        def normalize(a, b): return a / b
+
+        return normalize(self.v_normal, norm(*self.v_normal))
